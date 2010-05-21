6e66b00f8e2082e4ccb2bb7158143e3e9aed3a2f
Jan Drchal <dhonza@gmail.com>
2010-05-21 23:29:43 +0200

    started hypersneat

---
+++ src/hyper/evaluate/SNEATNetworkEvaluator.java
@@ -0,0 +1,42 @@
+package hyper.evaluate;
+
+import sneat.experiments.INetworkEvaluator;
+import sneat.neuralnetwork.INetwork;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: drchaj1
+ * Date: Apr 28, 2010
+ * Time: 10:39:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SNEATNetworkEvaluator implements INetworkEvaluator {
+//    private final float[][] in = {{1.0f, 0.0f, 0.0f}, // the first number for bias
+//            {1.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}};
+
+    private final float[][] in = {{0.0f, 0.0f},
+            {0.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}};
+
+    private final double[] out = {0.0, 1.0, 1.0, 0.0};
+
+    public double evaluateNetwork(INetwork network) {
+        float error = 0;
+        for (int i = 0; i < 4; i++) {
+            network.clearSignals();
+            network.setInputSignals(in[i]);
+            network.multipleSteps(5);
+            error += Math.abs(out[i] - network.getOutputSignal(0));
+        }
+        return Math.pow((4.0 - error), 2);
+    }
+
+    public double threadSafeEvaluateNetwork(INetwork network, Semaphore sem) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    public String getEvaluatorStateMessage() {
+        return "";
+    }
+}
--- src/hyper/evaluate/SNEATSolver.java
+++ src/hyper/evaluate/SNEATSolver.java
@@ -3,8 +3,12 @@ package hyper.evaluate;
 import common.pmatrix.ParameterCombination;
 import common.stats.Stats;
 import hyper.builder.NetSubstrateBuilder;
-import hyper.evaluate.printer.NetProgressPrinter1D;
-import neat.*;
+import neat.Genome;
+import neat.Net;
+import neat.Neuron;
+import sneat.evolution.EvolutionAlgorithm;
+import sneat.evolution.IdGenerator;
+import sneat.neatgenome.GenomeFactory;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,13 +21,13 @@ import neat.*;
 /**
  * mela by dostat z venku: parametry NEAT, progress printer (stejny pro GP)
  */
-public class NEATSolver implements Solver {
+public class SNEATSolver implements Solver {
     final private ParameterCombination parameters;
     final private NetSubstrateBuilder substrateBuilder;
     final private Stats stats;
     final private Problem problem;
 
-    public NEATSolver(ParameterCombination parameters, NetSubstrateBuilder substrateBuilder, Stats stats, Problem problem) {
+    public SNEATSolver(ParameterCombination parameters, NetSubstrateBuilder substrateBuilder, Stats stats, Problem problem) {
         this.parameters = parameters;
         this.substrateBuilder = substrateBuilder;
         this.stats = stats;
@@ -46,6 +50,35 @@ public class NEATSolver implements Solver {
         int inputsCPPN = 2 * substrateBuilder.getSubstrate().getMaxDimension();
         int outputsCPPN = substrateBuilder.getSubstrate().getNumOfConnections();
 
+        SNEATExperiment exp = new SNEATExperiment(parameters, inputsCPPN, outputsCPPN);
+        IdGenerator idgen = new IdGenerator();
+
+        EvolutionAlgorithm ea = new EvolutionAlgorithm(
+                new sneat.evolution.Population(idgen,
+                        GenomeFactory.CreateGenomeList(exp.getDefaultNeatParameters(),
+                                idgen,
+                                exp.getInputNeuronCount(),
+                                exp.getOutputNeuronCount(),
+                                exp.getDefaultNeatParameters().pInitialPopulationInterconnections,
+                                exp.getDefaultNeatParameters().populationSize)
+                ),
+                exp.getPopulationEvaluator(),
+                exp.getDefaultNeatParameters());
+
+
+        double maxFitness = -Double.MAX_VALUE;
+        for (int j = 0; j < exp.getDefaultNeatParameters().maxGenerations; j++) {
+            long dt = System.currentTimeMillis();
+            ea.performOneGeneration();
+            if (ea.getBestGenome().getFitness() > maxFitness) {
+                maxFitness = ea.getBestGenome().getFitness();
+//                XmlGenomeWriterStatic.Write(new File("bestGenome" + j + ".xml"), (NeatGenome) ea.getBestGenome());
+            }
+            System.out.println(ea.getGeneration() + " " + (maxFitness) + " " + (System.currentTimeMillis() - dt));
+        }
+
+
+        /*
         NEATEvaluator evaluator = new NEATEvaluator(substrateBuilder, problem);
 
         NEAT aNEAT = new NEAT();
@@ -63,5 +96,6 @@ public class NEATSolver implements Solver {
         aNEAT.run(false);
 
         stats.addSample("STAT_GENERATIONS", population.getGeneration());
+        */
     }
 }
