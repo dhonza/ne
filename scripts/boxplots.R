35108c4f1b1aebb722780603d657986f8ed010b3
Jan Drchal <dhonza@gmail.com>
2010-06-04 13:08:29 +0200

    updates in R scripts - boxplots

--- scripts/boxplots.R
+++ scripts/boxplots.R
@@ -1,3 +1,11 @@
 ea.data <- read.table("exp_all.txt", header=T)
+
+pdf("evaluations.pdf")
+boxplot(ea.data$EVALUATIONS ~ ea.data$EXP, col="bisque", notch=TRUE, range=0)
 #boxplot(ea.data$EVALUATIONS ~ ea.data$EXP, col="bisque", range=0)
-boxplot(ea.data$EVALUATIONS ~ ea.data$EXP, col="bisque")
+dev.off()
+
+pdf("evaluations_sorted.pdf")
+sortedByMedians <- reorder(ea.data$EXP, ea.data$EVALUATIONS, median)
+boxplot(ea.data$EVALUATIONS ~ sortedByMedians, col="bisque", notch=TRUE, range=0)
+dev.off()
