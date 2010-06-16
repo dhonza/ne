02f61320bc7783102984136da74013b5fac17700
Jan Drchal <dhonza@gmail.com>
2010-06-16 09:41:32 +0200

    started implementation of FindCluster

---
+++ src/hyper/experiments/findcluster/FindCluster.java
@@ -0,0 +1,23 @@
+package hyper.experiments.findcluster;
+
+import common.net.INet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: drchaj1
+ * Date: Jun 15, 2010
+ * Time: 6:36:32 PM
+ * Adapted from  Jason Gauci's HyperNEAT C++ 3.0
+ */
+public class FindCluster {
+    private int numNodesX;
+    private int numNodesY;
+
+    private int CELL_SIZE;
+
+    private int sizeMultiplier;
+
+    double processEvaluation(INet net, int x1, int y1, int x1Big, int y1Big) {
+        return 0.0;
+    }
+}
